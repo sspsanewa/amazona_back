2f36e69761cc7efb6ce58f4de1edfba80016e712
sandeep patidar <spsanewagit@gmail.com>
2024-09-18 02:40:57 +0530

    Initial commit for deployment

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
 import path from 'path';
-
+import { fileURLToPath } from 'url'; // Added this to mimic __dirname
 
 dotenv.config();
 
+// MongoDB connection
 mongoose.connect(process.env.MONGODB_URL).then(() => {
     console.log('Connected to db');
 }).catch((err) => {
@@ -22,25 +23,29 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Mimic __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// Serve static files from the client/build folder
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
-
+// Routes for API
 app.get('/api/keys/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
-
+// Start server
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => {
     console.log(`serve at http://localhost:${port}`);
 });
