0a27325938fa7806cb28e21a3b7b9bad9b613868
sandeep patidar <spsanewagit@gmail.com>
2024-09-18 01:59:18 +0530

    Initial commit for deployment

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import data from './data.js';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
+import orderRouter from './routes/orderRoutes.js';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log('Connected to db');
+}).catch((err) => {
+    console.log(err.message);
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/api/keys/paypal', (req, res) => {
+    res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`serve at http://localhost:${port}`);
+});
